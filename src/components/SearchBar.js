6959c121bcaecf0a02deccbdb2394d9758c19355
Eric Bryant <EBryant407@yahoo.com>
2021-04-15 20:48:14 -0400

    Init

---
+++ src/components/SearchBar.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { View, TextInput, StyleSheet } from 'react-native';
+import { FontAwesome5 } from '@expo/vector-icons';
+
+const SearchBar = () => {
+    return (
+        <View style={styles.backgroundStyle}>
+            <FontAwesome5 name='search' style={styles.iconStyle} />
+            <TextInput style={styles.inputStyle}
+                placeholder='search'
+            />
+        </View>
+    )
+};
+
+const styles = StyleSheet.create({
+    backgroundStyle: {
+        backgroundColor: '#F0EEEE',
+        height: 50,
+        borderRadius: 5,
+        marginHorizontal: 15,
+        flexDirection: 'row',
+        marginTop: 10,
+    },
+    inputStyle: {
+        flex: 1,
+        fontSize: 18,
+    },
+    iconStyle: {
+        fontSize: 35,
+        alignSelf: 'center',
+        marginHorizontal: 15,
+    }
+});
+
+export default SearchBar;
