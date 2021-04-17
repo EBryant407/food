0d706a204472a150ecf93e8283668d117bff07b7
Eric Bryant <EBryant407@yahoo.com>
2021-04-17 18:20:51 -0400

    Added error message

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -7,16 +7,21 @@ import yelp from '../api/yelp';
 const SearchScreen = () => {
     const [term, setTerm] = useState('');
     const [results, setResults] = useState([]);
+    const [errorMessage, setErrorMessage] = useState('');
 
     const searchApi = async () => {
-        const response = await yelp.get('/search', {
-            params: {
-                limit: 50,
-                term,
-                location: 'Orlando'
-            }
-        });
-        setResults (response.data.businesses);
+        try {
+            const response = await yelp.get('/search', {
+                params: {
+                    limit: 50,
+                    term,
+                    location: 'Orlando'
+                }
+            });
+            setResults(response.data.businesses);
+        } catch (err) {
+            setErrorMessage('Error. Try again later')
+        }
     };
 
     return (
@@ -26,9 +31,9 @@ const SearchScreen = () => {
                 onTermChange={setTerm}
                 onTermSubmit={searchApi}
             />
-            <Text>
-                Search Screen
-        </Text>
+            {errorMessage ? <Text>
+                {errorMessage}
+        </Text> : null}
             <Text>
                 We have found {results.length} results
         </Text>
